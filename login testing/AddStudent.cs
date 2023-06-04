e91d3662f16962dc8066958a539fa5903ab07ccc
blueorion05 <jhudielatienza2k20@gmail.com>
2023-06-04 22:18:12 +0800

    updated add student

--- login testing/AddStudent.cs
+++ login testing/AddStudent.cs
@@ -23,29 +23,32 @@ namespace login_testing
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string sql = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Path.Combine(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.FullName, "Database1.mdf") + ";Integrated Security=True";
-            SqlConnection conn = new SqlConnection(sql);
-            conn.Open();
-            try
+            if (textBox1.Text != "" && textBox2.Text != "")
             {
-                String match = "INSERT INTO Student (number, name) VALUES (@number, @name)";
-                SqlCommand cmd = new SqlCommand(match, conn);
-                cmd.CommandType = CommandType.Text;
-                cmd.Parameters.Add("@number", SqlDbType.VarChar).Value = textBox1.Text;
-                cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = textBox2.Text;
-                cmd.ExecuteNonQuery();
-                portal.RefreshStudent();
-                MessageBox.Show("Added");
-                textBox1.Text = "";
-                textBox2.Text = "";
-            }
-            catch
-            {
-                MessageBox.Show("Failed");
-            }
-            finally
-            {
-                conn.Close();
+                string sql = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Path.Combine(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.FullName, "Database1.mdf") + ";Integrated Security=True";
+                SqlConnection conn = new SqlConnection(sql);
+                conn.Open();
+                try
+                {
+                    String match = "INSERT INTO Student (number, name) VALUES (@number, @name)";
+                    SqlCommand cmd = new SqlCommand(match, conn);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add("@number", SqlDbType.VarChar).Value = textBox1.Text;
+                    cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = textBox2.Text;
+                    cmd.ExecuteNonQuery();
+                    portal.RefreshStudent();
+                    MessageBox.Show("Added");
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                }
+                catch
+                {
+                    MessageBox.Show("Failed");
+                }
+                finally
+                {
+                    conn.Close();
+                }
             }
         }
     }
